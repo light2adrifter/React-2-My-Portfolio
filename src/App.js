636ff256113524a81255cc0f61c055534a615f72
light <light2adrifter@yahoo.com>
2022-04-08 21:47:32 -0400

    workin in progress, react showing all black

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import About from './components/About';
+import Portfolio from './components/Portfolio'
 
 function App() {
+  const [categories] = useState([
+    { name: 'Portfolio', description: "What I've done" },
+    { name: 'Contact', description: "Reach out to me" },
+    { name: 'Resume', description: "What I'm made of" }
+]);
+
+const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav>
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      </Nav>
+      <main>
+        <Portfolio></Portfolio>
+        <About></About>
+      </main>
     </div>
   );
 }
---
+++ src/components/About/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import coverImage from "../../assets/cover/space.jpg";
+
+function About() {
+
+    return (
+        <section className='my-5'>
+            <h1 id='about'>"Towards the future"</h1>
+            <img src={coverImage} className='my-2' style={{ width: '100%'}} alt= 'cover' />
+        <div className='my-2'>
+            <p>Interesting stuffz about me
+            </p>
+        </div>
+        </section>
+    );
+};
+
+export default About;
